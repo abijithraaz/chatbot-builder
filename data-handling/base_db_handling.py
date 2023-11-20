4e23399ba8a714aca6c41976b5f2ad2f1e8b6de3
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-20 15:37:13 +0530

    Delete data-handling directory

--- data-handling/base_db_handling.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-class DataStoring(abc.ABC):
-
-    @abc.abstractmethod
-    def vectordata_storing(self, document_chunks):
-        """
-        Storing the embedded chunks
-        """
