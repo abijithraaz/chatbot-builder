c85a9bef59cdd62289b71bad1350c098c81afef6
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-20 20:24:43 +0530

    Update prompt_augmentor.py

--- prompt_augmentor.py
+++ prompt_augmentor.py
@@ -1,18 +1,7 @@
 import os
 from retriever.data_retriever import PineconeDBRetriever
-from langchain.prompts import ChatPromptTemplate
 from langchain.chains import RetrievalQA
-
-
-template = """You are an assistant for question-answering tasks. 
-Use the following pieces of retrieved context to answer the question. 
-If you don't know the answer, just say that you don't know. 
-Use three sentences maximum and keep the answer concise.
-Question: {question} 
-Context: {context} 
-Answer:
-"""
-prompt = ChatPromptTemplate.from_template(template)
+from langchain.chat_models import ChatOpenAI
 
 class PromptAugmentor:
     def __init__(self) -> None:
@@ -22,6 +11,7 @@ class PromptAugmentor:
         
         retriever = PineconeDBRetriever()
         vectorstore = retriever.retrieve_data(embedd_client=embedd)
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
         qa = RetrievalQA.from_chain_type(
         prompt=prompt,
@@ -29,4 +19,13 @@ class PromptAugmentor:
         chain_type="stuff",
         retriever=vectorstore.as_retriever())
 
-        return qa.run(query)
+        mod_query = f"""You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question. 
+            If you don't know the answer, just say that you don't know. 
+            Use three sentences maximum and keep the answer concise.
+            Question: {query} 
+            Context: 
+            Answer:
+            """
+
+        return qa.run(mod_query)
