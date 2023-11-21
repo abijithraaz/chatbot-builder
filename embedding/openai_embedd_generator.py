5108837b17bd8ca47d2ca4a3005e2b8ec07a9c52
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-21 12:53:29 +0530

    Update openai_embedd_generator.py

--- embedding/openai_embedd_generator.py
+++ embedding/openai_embedd_generator.py
@@ -5,7 +5,7 @@ from embedding.base_embedd_generator import BaseEmbeddGenarator
 
 class OpenaiEmbeddGenarator(BaseEmbeddGenarator):
     def __init__(self) -> None:
-        self.embed = OpenAIEmbeddings(model=os.getenv('OPENAI_EMBEDD_MODEL',''),openai_api_key=os.getenv('OPENAI_KEY',''))
+        self.embed = OpenAIEmbeddings(model=os.getenv('OPENAI_EMBEDD_MODEL',''),openai_api_key=os.getenv('OPENAI_API_KEY',''))
 
     def embedd_generator(self, data_frame):
         print(data_frame)
