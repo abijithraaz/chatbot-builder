4c8f9b91a824be6e228a73699b65193fc695db90
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-21 12:54:17 +0530

    Update openai_llm.py

--- llm_chat/openai_llm.py
+++ llm_chat/openai_llm.py
@@ -4,7 +4,7 @@ from llm_chat.base_llm import BaseLLM
 
 class OpenAiLLM(BaseLLM):
     def __init__(self) -> None:
-        self.client = OpenAI(api_key=os.getenv('OPENAI_KEY', ''))
+        self.client = OpenAI(api_key=os.getenv('OPENAI_AP_KEY', ''))
 
     def llmchat(self, prompt: str) -> str:
         chat_completion = self.client.chat.completions.create(
