d042af7420ba5b3bf5eef363c090936980dcd4d5
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-24 11:08:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ openai_chat = OpenAiLLM()
 rag_client = PromptAugmentor()
 embedd = OpenaiEmbeddGenarator()
 
+#sidebar
+with st.sidebar:
+    st.title('🤗💬 Chatter')
+
 # UI section
 st.header(body='Chatter', divider='rainbow')
 st.markdown(body= 'Chatter is also known as Chat Bot Builder')
