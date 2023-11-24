320cdb2cd346baf1dbb67b43f1ea4e247b660f5e
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-24 11:35:16 +0530

    Update base_db_handling.py

--- data_handling/base_db_handling.py
+++ data_handling/base_db_handling.py
@@ -11,7 +11,7 @@ class DataStoring(abc.ABC):
 class DBModify(abc.ABC):
 
     @abc.abstractmethod
-    def clear_db(self, sql_query):
+    def clear_db(self, sql_query=''):
         """
         Modifying the vector DB
         """
