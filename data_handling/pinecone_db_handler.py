14d6032fd2043fd5c3de21d59650141a5fdbeb92
Abijith <86768788+abijithraaz@users.noreply.github.com>
2023-11-24 12:46:46 +0530

    Update pinecone_db_handler.py

--- data_handling/pinecone_db_handler.py
+++ data_handling/pinecone_db_handler.py
@@ -10,17 +10,14 @@ class PineconeDB(DataStoring, DBModify):
         pinecone.init(api_key=os.getenv('PINECONE_API_KEY', ''), environment=os.getenv('PINECONE_ENVIRONMENT', ''))
 
     def vectordata_storing(self, document_chunks, embedd_obj):
-        vector_lists = []
         index = pinecone.Index(os.environ['PINECONE_INDEX_NAME'])
         # wait a moment for the index to be fully initialized
         time.sleep(1)
 
         # print('DocumentsChunks:',document_chunks)
         for ind, row in document_chunks.iterrows():
-            vector_data = (str(ind), row['embeddings'], {"text": row['text']})
-            # vector_data = {'id':str(ind), 'values': row['embeddings'],'metadata': {'text': row['text']}}
-            vector_lists.append(vector_data)
-        index.upsert(vector_lists)
+            vector_data = {'id':str(ind), 'values': row['embeddings'],'metadata': {'text': row['text']}}
+            index.upsert([vector_data])
         return True
 
     def clear_db(self, sql_query=''):
